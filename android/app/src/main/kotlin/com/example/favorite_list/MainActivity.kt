5ecdac357e514593f732d51150d0ac55799e6b72
suna0 <suna0.o4o4gym@ezweb.ne.jp>
2021-06-25 10:34:55 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/favorite_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.favorite_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:favorite_list/categories_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
